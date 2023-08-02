9d34a39e2fa789742f357715089073071a6239f1
LyanSpier <136393042+LyanSpier@users.noreply.github.com>
2023-08-02 20:47:10 -0300

    Add files via upload
    update

--- SENAC-FINANCE/core/setting.py
+++ SENAC-FINANCE/core/setting.py
@@ -2,3 +2,14 @@ from pathlib import Path
 import os
 
 BASE_DIR  = path
+
+
+
+
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
+STATIC_ROOT = os.path.join('static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
